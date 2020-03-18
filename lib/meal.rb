94aa8dd6dddede1ab57a2321135a8dafe652833b
Matt Kuras <mattkuras@gmail.com>
2020-03-18 21:04:48 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,21 @@
 class Meal
+  
+  attr_accessor :water, :customer. :total, :tip 
+  
+  @@all = []
+  
+  def initialize(waiter, customer, total, tip=0)
+    @waiter = waiter 
+    @customer = customer 
+    @total = total 
+    @tip = tip
+    @@all << self 
+  end
+  
+  def self.all
+    @@all 
+  end
+  
+  
 
 end
